6329353277c1b13585d8e0e50f44280466f9e2ea
Axuby <tochukwu.azubuine.237253@unn.edu.ng>
2020-05-07 20:48:32 +0100

    Handled mongoose errors global

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,23 @@
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`
+  return new AppError(message,400)
+}
+
+const handleDuplicateFieldsDB =  (err) =>{
+  const value = err.ermsg.match(/(["'])(\\?.)*?\1/)[0]
+const message = `Duplicate field value: ${value}.Please use another value`;
+console.log(value)
+return new AppError(message , 400)
+}
+const handleValidationErrorDB = err => {
+
+  const errors = Object.values(err.errors).map(el => el.message)
+  const message = `Invalid input date.${errors.join('. ')}`
+  return new AppError(message,400)
+}
+
+
+
 
   const sendErrorProd = (err,res) =>{
     //operatinal error
@@ -7,9 +27,6 @@
         message:err.message 
       });
     }else{
-      //code or unknown error
-      console.error('Error',err);
-      
       res.status(500).json({
         status:'Error',
         message:'Something went very wrong!'
@@ -38,9 +55,21 @@ module.exports = (err,req, res, next) => {
   
 
  if (process.env.NODE_ENV === "development") {
+
 sendErrorDev(err,res)
 
  }else if(process.env.NODE_ENV === 'production'){
+   //cprogramming code or unknown error
+   console.error('Error',err);
+   //error handling for mongoose using the name key of err
+         let error = {...err};
+   
+   
+         if (error.name === 'CastError')   error = handleCastErrorDB(error)
+         //from postman
+         if (error.code === 11000)    error = handleDuplicateFieldsDB(error) 
+         if (error.name === 'ValidationError')   error = handleValidationErrorDB(error)
+
  sendErrorProd(err,res)
  }
   next(
