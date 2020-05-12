99036aa41db7ea69d10d45e96823c36ec059560e
Axuby <tochukwu.azubuine.237253@unn.edu.ng>
2020-05-12 21:19:14 +0100

    Referenced User model

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require('./userMode;l')
 const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
@@ -70,6 +71,29 @@ createdAt:{
   select:false
 },
 startDates:[Date],
+startLocation:{//GeoJSON
+  type:{
+    type:String,
+    default:'point',
+    enum:['point']
+  },
+  coordinates:[Number],//longitude and lat instead of vice versa
+},
+  location :[{//GeoJSON
+    type:{
+      type:String,
+      default:'point',
+      enum:['point']
+    },
+    coordinates:[Number],
+    day:Number
+}],
+// guides:Array,
+guides:[
+  {
+    type:mongoose.Schema.ObjectId
+  }
+],
 summary:{
   type:String,
   trim:true,
@@ -89,10 +113,13 @@ tourSchema.virtual('durationWeeks').get(function(){
 
 //b4 .save() and .create() but not insertMany()
 tourSchema.pre('save',function(next){ //this is pointing to the currently saved Doc
-
   this.slug = slugify(this.name, {lower:true})
   next()
 })
+// tourSchema.pre('save ',async function (next) {
+//  const guidesPromises =  this.guides.map(async (id)=> await User.findById(id))
+//  this.guides = await Promise.all(guidesPromises)
+// })
 
 tourSchema.pre(/^find/,function(next){
   this.find({secretTour:{ $ne:true}})
